e882433eca3b728faef3ed67c3ac3d1a6c365d25
SHOMANS <mohammedshoman9@gmail.com>
2023-08-24 15:07:17 +0300

    remove useless main layout and keep the contexts inside the root layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,24 +1,48 @@
-import MainLayout from '@/components/layouts/MainLayout';
-import '../style/globals.css';
+'use client';
+
+import { ThemeProvider as ContextThemeProvider } from '@/contexts/themeContext';
+import { ThemeProvider } from 'styled-components';
 import { Inter } from 'next/font/google';
+import { darkTheme, theme } from '@/styles/theme';
+import GlobalStyle from '@/styles/global';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata = {
-  title: 'GSG',
-  description: 'description',
-};
-
 export default function RootLayout({ children }) {
+  // const { theme } = useThemeContext(); // won't work
+  // const myCB = (myTheme) => (
+  //   <ThemeProvider theme={myTheme === 'light' ? theme : darkTheme}>
+  //     <h1>hello from root</h1>
+  //     <div style={{ margin: '0 auto', width: '90%', padding: 20 }}>
+  //       {children}
+  //     </div>
+  //   </ThemeProvider>
+  // ); // same as children
+
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <MainLayout>
-          <h1>hello from root</h1>
-          <div style={{ margin: '0 auto', width: '90%', padding: 20 }}>
-            {children}
-          </div>
-        </MainLayout>
+        <ContextThemeProvider
+        // myCB={myCB}
+        // children={(myTheme) => (
+        //   <ThemeProvider theme={myTheme === 'light' ? theme : darkTheme}>
+        //     <h1>hello from root</h1>
+        //     <div style={{ margin: '0 auto', width: '90%', padding: 20 }}>
+        //       {children}
+        //     </div>
+        //   </ThemeProvider>
+        // )}
+        >
+          {(myTheme) => (
+            <ThemeProvider theme={myTheme === 'light' ? theme : darkTheme}>
+              <GlobalStyle />
+              <h1>hello from root</h1>
+              <div style={{ margin: '0 auto', width: '90%', padding: 20 }}>
+                {children}
+              </div>
+            </ThemeProvider>
+          )}
+        </ContextThemeProvider>
       </body>
     </html>
   );
