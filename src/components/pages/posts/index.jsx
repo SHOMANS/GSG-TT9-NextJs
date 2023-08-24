2f83567d21142ce1ae8ecdfd437d089dc28d5e6b
SHOMANS <mohammedshoman9@gmail.com>
2023-08-24 15:08:46 +0300

    use theme inside a component using the useTheme hook

--- src/components/pages/posts/index.jsx
+++ src/components/pages/posts/index.jsx
@@ -2,9 +2,11 @@
 import { posts } from '@/mock/posts';
 import { useRouter } from 'next/navigation';
 import React from 'react';
+import { useTheme } from 'styled-components';
 
 const PostsPage = () => {
   const router = useRouter();
+  const theme = useTheme();
 
   return (
     <div>
@@ -15,7 +17,13 @@ const PostsPage = () => {
             key={post.id}
             style={{ margin: '16px 0', cursor: 'pointer' }}
           >
-            <h2>{post.title}</h2>
+            <h2
+              style={{
+                color: theme.colors.secondary,
+              }}
+            >
+              {post.title}
+            </h2>
             <p>{post.body}</p>
           </div>
         );
